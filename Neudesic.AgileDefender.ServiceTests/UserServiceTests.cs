31e9a93cfde8b630fe29b9c73e13651ff3636268
Chris Palmer <palmerc7@att.net>
2015-06-05 02:29:36 -0500

    Unit tests aync calls to User service

---
+++ Neudesic.AgileDefender.ServiceTests/UserServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Neudesic.AgileDefender.ServiceTests
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        private string baseUrl = "https://agiledefenderservices.azure-mobile.net/api";
+        private HttpClient client = new HttpClient();
+
+        [TestMethod]
+        public void UserServiceCanValidate()
+        {
+            Console.WriteLine("Start");
+
+            var task = new Task(() =>
+            {
+                var result = ValidateUserService().Wait(1000);
+                Assert.AreNotEqual(string.Empty, result);
+                Console.WriteLine(result);
+            });
+
+            Console.WriteLine("End");
+        }
+
+        private async Task<string> ValidateUserService()
+        {
+            try
+            {
+                var url = string.Format("{0}/v1/user/validate", baseUrl);
+                var result = await client.GetStringAsync(url);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                return null;
+            }
+        }
+
+    }
+}
