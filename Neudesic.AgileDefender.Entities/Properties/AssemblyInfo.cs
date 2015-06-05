e69791fd3c8fa36092da364b2b3de6a72b567bf8
Chris Palmer <palmerc7@att.net>
2015-06-04 21:28:34 -0500

    Added Entities project. Added App Insights library

--- Neudesic.AgileDefender.Entities/Properties/AssemblyInfo.cs
+++ Neudesic.AgileDefender.Entities/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Neudesic.AgileDefender.Storm")]
+[assembly: AssemblyTitle("Neudesic.AgileDefender.Entities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GameStop, Inc.")]
-[assembly: AssemblyProduct("Neudesic.AgileDefender.Storm")]
-[assembly: AssemblyCopyright("Copyright © GameStop, Inc. 2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Neudesic.AgileDefender.Entities")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("19ca0aed-0916-49b6-9b2d-e76cc1d6e87f")]
+[assembly: Guid("effe7cc1-0e92-47a4-a1e1-75f0c7c094ab")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Neudesic.AgileDefender.Services/Processors/UserProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Neudesic.AgileDefender.Services.DataObjects;
+
+namespace Neudesic.AgileDefender.Services.Processors
+{
+    public class UserProcessor
+    {
+        public User GetUserByEmail(string emailAddress)
+        {
+            // TODO returning fake data for now
+            var user = new User
+            {
+                Name = "Chris Palmer",
+                Email = "chris.palmer@neudesic.com",
+                IsSuccess = true
+            };
+
+            return user;
+        }
+    }
+}
