afce5fb95a1995464c43697a06a1e16932739e24
Chris Palmer <palmerc7@att.net>
2015-06-09 16:34:11 -0500

    Additional Api tests

---
+++ Neudesic.AgileDefender.ServiceTests/FaceOffersTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neudesic.AgileDefender.ServiceTests
+{
+    [TestClass]
+    public class FaceOffersTests
+    {
+        private string baseUrl = "https://api.faceoffers.com/api";
+        private string authApiUrl;
+        private string getUserApiUrl;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            authApiUrl = string.Format("{0}/Apps/Authenticate", baseUrl);
+            //getUserApiUrl = string.Format("{0}/Users/ByUsername/?username={1}", baseUrl, "palmerc7");
+            getUserApiUrl = string.Format("{0}/Users/ByUsername/{1}", baseUrl, "palmerc7");
+        }
+
+        [TestMethod]
+        public void CanPostToGetAuthToken()
+        {
+            var task = Task.Run(async () =>
+            {
+                var result = await GetAuthToken();
+                Assert.AreNotEqual(string.Empty, result);
+                //Assert.IsTrue(result.ToLower().Contains("success"));
+            });
+
+            task.Wait();
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void CanGetUserByName()
+        {
+            var task = Task.Run(async () =>
+            {
+                var result = await GetUserService();
+                Assert.AreNotEqual(string.Empty, result);
+                //Assert.IsTrue(result.ToLower().Contains("success"));
+            });
+
+            task.Wait();
+        }
+
+        private async Task<string> GetAuthToken()
+        {
+            var result = string.Empty;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    //HttpContent content = new StringContent("{ 'ApiKey': 'Mb7j6YSg84Bon2N9KtZe35Xms2P4Ryp8L3CrQi76Tfz9H5Wda5ADq6k7G2ExFw94' }");
+                    
+                    var postData = new List<KeyValuePair<string, string>>();
+                    postData.Add(new KeyValuePair<string, string>("ApiKey", "Mb7j6YSg84Bon2N9KtZe35Xms2P4Ryp8L3CrQi76Tfz9H5Wda5ADq6k7G2ExFw94"));
+                    HttpContent content = new FormUrlEncodedContent(postData);
+
+                    //client.PostAsync(authApiUrl, content).ContinueWith(
+                    //(postTask) =>
+                    //{
+                        //postTask.Result.EnsureSuccessStatusCode();
+                    //});
+
+                    HttpResponseMessage response = await client.PostAsync(authApiUrl, content);
+                    var rr = response;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("ValidateUserService ERROR: {0}"), ex.Message);
+                return result;
+            }
+        }
+
+        private async Task<string> GetUserService()
+        {
+            var result = string.Empty;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    result = await client.GetStringAsync(getUserApiUrl);
+                    var rr = result;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("ValidateUserService ERROR: {0}"), ex.Message);
+                return result;
+            }
+        }
+
+    }
+}
